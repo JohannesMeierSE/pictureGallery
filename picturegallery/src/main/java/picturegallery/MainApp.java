29cf470310dafe7a8950cad64081b041c7912777
Johannes Meier <johannes.meier@uni-oldenburg.de>
2016-11-04 20:48:20 +0100

    simplified the generated JavaFX stuff

--- picturegallery/src/main/java/picturegallery/MainApp.java
+++ picturegallery/src/main/java/picturegallery/MainApp.java
@@ -1,32 +1,22 @@
 package picturegallery;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class MainApp extends Application {
-
-    private static final Logger log = LoggerFactory.getLogger(MainApp.class);
 
     public static void main(String[] args) throws Exception {
         launch(args);
     }
 
     public void start(Stage stage) throws Exception {
+    	Label label = new Label("Hello!");
+    	StackPane root = new StackPane(label);
 
-        log.info("Starting Hello JavaFX and Maven demonstration application");
-
-        String fxmlFile = "/fxml/hello.fxml";
-        log.debug("Loading FXML for main view from: {}", fxmlFile);
-        FXMLLoader loader = new FXMLLoader();
-        Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
-
-        log.debug("Showing JFX scene");
-        Scene scene = new Scene(rootNode, 400, 200);
+        Scene scene = new Scene(root, 400, 200);
         scene.getStylesheets().add("/styles/styles.css");
 
         stage.setTitle("Hello JavaFX and Maven");
