0504658483be3fbdc182bdcfcf19072842124f20
Johannes Meier <johannes.meier@uni-oldenburg.de>
2016-11-04 21:52:57 +0100

    finished version version with showing a picture

--- picturegallery/src/main/java/picturegallery/MainApp.java
+++ picturegallery/src/main/java/picturegallery/MainApp.java
@@ -1,17 +1,30 @@
 package picturegallery;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import picturegallery.persistency.Settings;
 
 public class MainApp extends Application {
 
@@ -28,16 +41,58 @@ public class MainApp extends Application {
     	BorderPane root = new BorderPane();
 
     	ImageView iv = new ImageView();
+    	iv.setPreserveRatio(true);
+    	iv.setSmooth(true);
+    	iv.setCache(true);
+    	iv.setFitWidth(100);
     	root.setCenter(iv);
 
     	VBox left = new VBox();
     	Label label = new Label("Hello!");
     	left.getChildren().add(label);
-    	Button but = new Button("pp");
+    	Button but = new Button("Load");
     	but.setOnAction(new  EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				System.out.println("pressed");
+		        List<String> pictures = new ArrayList<>();
+		        // https://stackoverflow.com/questions/1844688/read-all-files-in-a-folder/23814217#23814217
+		        String baseDir = Settings.getBasePath();
+		        System.out.println(baseDir);
+		        try {
+					Files.walkFileTree(Paths.get(baseDir), new SimpleFileVisitor<Path>() {
+					    @Override
+						public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+					    	// ignore sub-folders, but accept the initial base path!
+					    	if (dir.toString().equals(baseDir)) {
+					    		return FileVisitResult.CONTINUE;
+					    	}
+							return FileVisitResult.SKIP_SUBTREE;
+						}
+
+						@Override
+					    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+							String name = file.toString();
+					        System.out.println("file: " + name);
+					        if (name.endsWith(".png") || name.endsWith(".jpg")) {
+					        	pictures.add(file.toString());
+					        }
+					        return FileVisitResult.CONTINUE;
+					    }
+					});
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		        if (!pictures.isEmpty()) {
+					Image im = null;
+					try {
+						im = new Image(new File(pictures.get(0)).toURI().toURL().toString());
+					} catch (MalformedURLException e) {
+						e.printStackTrace();
+					}
+					iv.setImage(im);
+		        } else {
+		        	System.out.println("no pictures found!");
+		        }
 			}
 		});
     	left.getChildren().add(but);
@@ -48,7 +103,7 @@ public class MainApp extends Application {
 
     	// https://stackoverflow.com/questions/23163189/keylistener-javafx
     	// https://stackoverflow.com/questions/16834997/cannot-listen-to-keyevent-in-javafx
-    	scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+    	scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
     		@Override
     		public void handle(KeyEvent event) {
     			System.out.println(event.getText());
---
+++ picturegallery/src/main/java/picturegallery/persistency/Settings.java
@@ -0,0 +1,7 @@
+package picturegallery.persistency;
+
+public class Settings {
+	public static String getBasePath() {
+		return "/home/johannes/Documents/photo/pictureGallery/picturegallery/src/main/resources/images";
+	}
+}
