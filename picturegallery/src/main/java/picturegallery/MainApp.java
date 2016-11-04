d9495b4e485ff0c5560ab62c36b0aef58053b538
Johannes Meier <johannes.meier@uni-oldenburg.de>
2016-11-04 21:17:35 +0100

    added support for key pressing

--- picturegallery/src/main/java/picturegallery/MainApp.java
+++ picturegallery/src/main/java/picturegallery/MainApp.java
@@ -1,9 +1,16 @@
 package picturegallery;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
@@ -12,14 +19,44 @@ public class MainApp extends Application {
         launch(args);
     }
 
+    /*
+     * https://github.com/javafx-maven-plugin/javafx-basic-archetype
+     * https://github.com/javafx-maven-plugin/javafx-maven-plugin
+     * create jar: run "mvn jfx:jar"
+     */
     public void start(Stage stage) throws Exception {
+    	BorderPane root = new BorderPane();
+
+    	ImageView iv = new ImageView();
+    	root.setCenter(iv);
+
+    	VBox left = new VBox();
     	Label label = new Label("Hello!");
-    	StackPane root = new StackPane(label);
+    	left.getChildren().add(label);
+    	Button but = new Button("pp");
+    	but.setOnAction(new  EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("pressed");
+			}
+		});
+    	left.getChildren().add(but);
+    	root.setLeft(left);
 
-        Scene scene = new Scene(root, 400, 200);
-        scene.getStylesheets().add("/styles/styles.css");
+    	Scene scene = new Scene(root, 400, 200);
+    	scene.getStylesheets().add("/styles/styles.css");
 
-        stage.setTitle("Hello JavaFX and Maven");
+    	// https://stackoverflow.com/questions/23163189/keylistener-javafx
+    	// https://stackoverflow.com/questions/16834997/cannot-listen-to-keyevent-in-javafx
+    	scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+    		@Override
+    		public void handle(KeyEvent event) {
+    			System.out.println(event.getText());
+    			label.setText(event.getText());
+    		}
+    	});
+
+        stage.setTitle("Picture Gallery");
         stage.setScene(scene);
         stage.show();
     }
